c49b7f1df63e96e956e2425b947ccea8a96b7af9
sergey <molokoyt@bk.ru>
2026-03-22 21:47:50 +0700

    Добавили вычисление площади, периметра, диагонали прямоугольника

---
+++ laba1.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main{
+
+       	double side1, side2; 
+	double square, perimeter, diagonal; 
+	
+	cin >> side1 >> side2;// введем стороны прямоуголька 
+	
+	if (side1 <= 0 || side2 <= 0){ //защита от отрицательных чисел
+	cout << "Ошибка!" <<endl; 
+	return 0; 
+	} 
+	
+	square = (side1 + side2)*2; 
+	perimeter = side1 * side2; 
+	diagonal = sqrt( pow(side1, 2) + pow(side2, 2) ); 
+	
+	cout << "Периметр = " << perimeter << endl; 
+	cout << "Площадь = " << square << endl; 
+	cout << "Диагональ = " << diagonal<< endl; 
+	return 0; 
+}
